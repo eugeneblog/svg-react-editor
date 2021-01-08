6524e3c82c277a242cf096dc400199420f6a57e3
eugeneblog <714284150@qq.com>
2021-01-08 10:48:04 +0800

    'init'

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@
 import Editor from './components/Editor';
 import Flow from './components/Flow';
 import ItemPane from './components/ItemPane';
+import Menu from './components/Menu';
+import Tools from './components/Tools';
 import './app.less';
 import 'antd/dist/antd.css';
 
-export { Flow, ItemPane };
+export { Flow, ItemPane, Menu, Tools };
 
 export default Editor;
